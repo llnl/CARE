86fa1f17c0d0fe4057470ecc2667c8c8c3dc6e03
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2026-06-02 14:41:26 -0700

    Update host configs to use newer compilers (#373)

--- host-configs/lc/toss_4_x86_64_ib/clang_tsan.cmake
+++ host-configs/lc/toss_4_x86_64_ib/clang_tsan.cmake
@@ -5,14 +5,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 ##############################################################################
 
-set(COMPILER_BASE "/usr/tce/packages/clang/clang-14.0.6-magic" CACHE PATH "")
+set(COMPILER_BASE "/usr/tce/packages/clang/clang-19.1.3-magic" CACHE PATH "")
 set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/clang++" CACHE PATH "")
 
 set(GCC_HOME "/usr/tce/packages/gcc/gcc-13.3.1-magic" CACHE PATH "")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread -g" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread -g" CACHE STRING "")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=${GCC_HOME} -fsanitize=thread" CACHE STRING "")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=thread" CACHE STRING "")
+set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "")
 
 set(ENABLE_OPENMP ON CACHE BOOL "")
 set(BLT_EXPORT_THIRDPARTY OFF CACHE BOOL "")
