bade64a1aeed10251a99dccfeb23f57585e68085
Alan Dayton <dayton8@llnl.gov>
2026-06-04 15:32:08 -0700

    Clean up

--- examples/DeviceASAN.cpp
+++ examples/DeviceASAN.cpp
@@ -21,7 +21,7 @@ int main(int, char**) {
    //
    // ==4084715==ERROR: AddressSanitizer: heap-buffer-overflow on amdgpu device 0 at pc 0x153d3cdd3db8
    // READ of size 4 in workgroup id (3906,0,0)
-   //   #0 0x153d3cdd3db8 in main::'lambda0'(int)::operator()(int) const at /g/g17/dayton8/ale3d/github_care_4/examples/DeviceASAN.cpp:26:14
+   //   #0 0x153d3cdd3db8 in main::'lambda0'(int)::operator()(int) const at care/examples/DeviceASAN.cpp:26:14
    CARE_STREAM_LOOP(i, 0, size + 1) {
       data[i]++;
    } CARE_STREAM_LOOP_END
