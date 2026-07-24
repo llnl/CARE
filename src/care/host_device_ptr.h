a59a7158c447ef652e66109bacd62ca228252fca
Alan Dayton <dayton8@llnl.gov>
2026-07-24 14:29:03 -0700

    Revert "Resolve overload ambiguity"
    This reverts commit 39e8a8f1c9ff5c24de16f314d02a6872af618058.

--- src/care/host_device_ptr.h
+++ src/care/host_device_ptr.h
@@ -21,7 +21,6 @@
 #include "chai/ManagedArray.hpp"
 
 // Std library headers
-#include <concepts>
 #include <cstddef>
 
 
@@ -94,16 +93,9 @@ namespace care {
       ///
       /// @author Peter Robinson
       ///
-      /// Construct from nullptr
+      /// nullptr constructor
       ///
-      /// @note The constraint prevents overload ambiguity between this
-      ///       constructor and the size_t constructor when constructing
-      ///       with the integer literal 0. Additionally, the constraint
-      ///       is a workaround for a nvcc bug where overload resolution
-      ///       can incorrectly report an ambiguity between this constructor
-      ///       and the size_t constructor.
-      ///
-      CARE_HOST_DEVICE host_device_ptr(std::same_as<std::nullptr_t> auto from) noexcept : MA (from) {}
+      CARE_HOST_DEVICE host_device_ptr(std::nullptr_t from) noexcept : MA (from) {}
 
       ///
       /// Construct from a size
