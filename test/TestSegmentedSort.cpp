a0c9c823e89e49b1a2d667a6a68d869ce9d09213
Alan Dayton <dayton8@llnl.gov>
2026-09-04 11:57:05 -0700

    Fixes for segmented sort

--- test/TestSegmentedSort.cpp
+++ test/TestSegmentedSort.cpp
@@ -12,16 +12,22 @@
 
 TEST(segmented_sort, segment_local_and_empty)
 {
-#ifdef CARE_GPUCC
-   init_care_for_testing();
-#endif
-
    care::host_device_ptr<int> keys(8);
    care::host_device_ptr<int> offsets(5);
 
-   const int input[] = {5, 1, 4, 9, 3, 8, 7, 2};
+   const int input[] = {
+      5, 1, 4,
+      // empty segment
+      9, 3, 8,
+      7, 2
+   };
    const int segmentOffsets[] = {0, 3, 3, 6, 8};
-   const int expected[] = {1, 4, 5, 3, 8, 9, 2, 7};
+   const int expected[] = {
+      1, 4, 5,
+      // empty segment
+      3, 8, 9,
+      2, 7
+   };
 
    CARE_SEQUENTIAL_LOOP(i, 0, 8) {
       keys[i] = input[i];
@@ -41,12 +47,36 @@ TEST(segmented_sort, segment_local_and_empty)
    keys.free();
 }
 
+TEST(segmented_sort, preserves_slice)
+{
+   care::host_device_ptr<int> storage(6);
+   care::host_device_ptr<int> keys = storage.slice(1, 4);
+   care::host_device_ptr<int> offsets(3);
+
+   const int input[] = {-1, 4, 2, 5, 3, -2};
+   const int segmentOffsets[] = {0, 2, 4};
+   const int expected[] = {-1, 2, 4, 3, 5, -2};
+
+   CARE_SEQUENTIAL_LOOP(i, 0, 6) {
+      storage[i] = input[i];
+   } CARE_SEQUENTIAL_LOOP_END
+
+   CARE_SEQUENTIAL_LOOP(i, 0, 3) {
+      offsets[i] = segmentOffsets[i];
+   } CARE_SEQUENTIAL_LOOP_END
+
+   care::segmented_sort(keys, offsets);
+
+   CARE_SEQUENTIAL_LOOP(i, 0, 6) {
+      EXPECT_EQ(storage[i], expected[i]);
+   } CARE_SEQUENTIAL_LOOP_END
+
+   offsets.free();
+   storage.free();
+}
+
 TEST(segmented_sort, empty_input)
 {
-#ifdef CARE_GPUCC
-   init_care_for_testing();
-#endif
-
    care::host_device_ptr<int> keys;
    care::host_device_ptr<int> offsets(1);
 
@@ -59,4 +89,15 @@ TEST(segmented_sort, empty_input)
 
    EXPECT_EQ(keys.size(), 0);
    EXPECT_EQ(keys.data(), nullptr);
+}
+
+int main(int argc, char** argv)
+{
+   testing::InitGoogleTest(&argc, argv);
+
+#ifdef CARE_GPUCC
+   init_care_for_testing();
+#endif
+
+   return RUN_ALL_TESTS();
 }
