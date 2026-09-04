6bf7e9b1dfc5664199179ed87bc181d1db35a678
Alan Dayton <dayton8@llnl.gov>
2026-09-04 11:11:19 -0700

    Add slice test for segmented sort

--- test/TestSegmentedSort.cpp
+++ test/TestSegmentedSort.cpp
@@ -37,6 +37,34 @@ TEST(segmented_sort, segment_local_and_empty)
    keys.free();
 }
 
+TEST(segmented_sort, preserves_slice)
+{
+   care::host_device_ptr<int> storage(6);
+   care::host_device_ptr<int> keys = storage.slice(1, 4);
+   care::host_device_ptr<int> offsets(3);
+
+   const int input[] = {-1, 4, 2, 5, 3, -2};
+   const int segmentOffsets[] = {0, 2, 4};
+   const int expected[] = {-1, 2, 4, 3, 5, -2};
+
+   CARE_SEQUENTIAL_LOOP(i, 0, 6) {
+      storage[i] = input[i];
+   } CARE_SEQUENTIAL_LOOP_END
+
+   CARE_SEQUENTIAL_LOOP(i, 0, 3) {
+      offsets[i] = segmentOffsets[i];
+   } CARE_SEQUENTIAL_LOOP_END
+
+   care::segmented_sort(keys, offsets);
+
+   CARE_SEQUENTIAL_LOOP(i, 0, 6) {
+      EXPECT_EQ(storage[i], expected[i]);
+   } CARE_SEQUENTIAL_LOOP_END
+
+   offsets.free();
+   storage.free();
+}
+
 TEST(segmented_sort, empty_input)
 {
    care::host_device_ptr<int> keys;
