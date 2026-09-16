d366cdc0c08e166a23bd287f9dddbe1324a8a2f6
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2026-09-16 09:43:10 -0700

    Add more sort methods (#408)
    * Add paired_sort and segmented_paired_sort

--- src/care/cuda/sort.h
+++ src/care/cuda/sort.h
@@ -20,6 +20,112 @@
 namespace care::cuda {
 
 /**
+ * @brief Sort keys in ascending order.
+ * @param keys Keys to sort in place.
+ */
+template <typename KeyT>
+CARE_INLINE void sort(care::host_device_ptr<KeyT>& keys)
+{
+   const size_t numItems = keys.size();
+
+   if (numItems == 0) {
+      return;
+   }
+
+   CHAIDataGetter<KeyT, RAJADeviceExec> keyGetter {};
+   auto* rawKeys = keyGetter.getRawArrayData(keys);
+
+   care::host_device_ptr<KeyT> result(numItems);
+   auto* rawResult = keyGetter.getRawArrayData(result);
+
+   size_t tempStorageBytes = 0;
+   cub::DeviceRadixSort::SortKeys(nullptr, tempStorageBytes,
+                                  rawKeys, rawResult, numItems);
+
+   CHAIDataGetter<char, RAJADeviceExec> charGetter {};
+   care::host_device_ptr<char> tempStorage(tempStorageBytes);
+   auto* rawTempStorage = charGetter.getRawArrayData(tempStorage);
+   cub::DeviceRadixSort::SortKeys(rawTempStorage, tempStorageBytes,
+                                  rawKeys, rawResult, numItems);
+
+   tempStorage.free();
+
+   if (keys.isSlice()) {
+      care::host_device_ptr<const KeyT> source = result;
+
+      CARE_STREAM_LOOP(i, 0, numItems) {
+         keys[i] = source[i];
+      } CARE_STREAM_LOOP_END
+
+      result.free();
+   } else {
+      keys.free();
+      keys = std::move(result);
+   }
+}
+
+/**
+ * @brief Sort keys in ascending order and apply the same permutation to values.
+ * @param keys Keys to sort in place.
+ * @param values Values associated one-to-one with keys. Must have the same
+ * number of entries as keys.
+ */
+template <typename KeyT, typename ValueT>
+CARE_INLINE void paired_sort(care::host_device_ptr<KeyT>& keys,
+                             care::host_device_ptr<ValueT>& values)
+{
+   const size_t numItems = keys.size();
+
+   if (numItems == 0) {
+      return;
+   }
+
+   CHAIDataGetter<KeyT, RAJADeviceExec> keyGetter {};
+   auto* rawKeys = keyGetter.getRawArrayData(keys);
+
+   CHAIDataGetter<ValueT, RAJADeviceExec> valueGetter {};
+   auto* rawValues = valueGetter.getRawArrayData(values);
+
+   care::host_device_ptr<KeyT> keyResult(numItems);
+   auto* rawKeyResult = keyGetter.getRawArrayData(keyResult);
+
+   care::host_device_ptr<ValueT> valueResult(numItems);
+   auto* rawValueResult = valueGetter.getRawArrayData(valueResult);
+
+   size_t tempStorageBytes = 0;
+   cub::DeviceRadixSort::SortPairs(nullptr, tempStorageBytes,
+                                   rawKeys, rawKeyResult,
+                                   rawValues, rawValueResult, numItems);
+
+   CHAIDataGetter<char, RAJADeviceExec> charGetter {};
+   care::host_device_ptr<char> tempStorage(tempStorageBytes);
+   auto* rawTempStorage = charGetter.getRawArrayData(tempStorage);
+   cub::DeviceRadixSort::SortPairs(rawTempStorage, tempStorageBytes,
+                                   rawKeys, rawKeyResult,
+                                   rawValues, rawValueResult, numItems);
+
+   tempStorage.free();
+
+   if (keys.isSlice() || values.isSlice()) {
+      care::host_device_ptr<const KeyT> keySource = keyResult;
+      care::host_device_ptr<const ValueT> valueSource = valueResult;
+
+      CARE_STREAM_LOOP(i, 0, numItems) {
+         keys[i] = keySource[i];
+         values[i] = valueSource[i];
+      } CARE_STREAM_LOOP_END
+
+      keyResult.free();
+      valueResult.free();
+   } else {
+      keys.free();
+      values.free();
+      keys = std::move(keyResult);
+      values = std::move(valueResult);
+   }
+}
+
+/**
  * @brief Sort keys in ascending order independently within each segment.
  * @param keys Keys to sort in place.
  * @param offsets Segment boundaries. For N segments, offsets must contain
@@ -74,6 +180,82 @@ CARE_INLINE void segmented_sort(care::host_device_ptr<KeyT>& keys,
    } else {
       keys.free();
       keys = std::move(result);
+   }
+}
+
+/**
+ * @brief Sort keys independently within each segment and apply the same
+ * permutation to values.
+ * @param keys Keys to sort in place.
+ * @param values Values associated one-to-one with keys. Must have the same
+ * number of entries as keys.
+ * @param offsets Segment boundaries. For N segments, offsets must contain
+ * N + 1 entries: offsets[i] begins segment i, and offsets[N] marks the end of
+ * the final segment. Segment i is therefore [offsets[i], offsets[i + 1]).
+ * The entries must form a nondecreasing sequence from 0 to keys.size(); that
+ * is, offsets[0] must be 0 and offsets[N] must equal keys.size(). Repeated
+ * entries denote empty segments.
+ */
+template <typename KeyT, typename ValueT, typename OffsetT>
+CARE_INLINE void segmented_paired_sort(
+   care::host_device_ptr<KeyT>& keys,
+   care::host_device_ptr<ValueT>& values,
+   care::host_device_ptr<OffsetT> const& offsets)
+{
+   const size_t numSegments = offsets.size() > 0 ? offsets.size() - 1 : 0;
+   const size_t numItems = keys.size();
+
+   if (numSegments == 0 || numItems == 0) {
+      return;
+   }
+
+   CHAIDataGetter<KeyT, RAJADeviceExec> keyGetter {};
+   auto* rawKeys = keyGetter.getRawArrayData(keys);
+
+   CHAIDataGetter<ValueT, RAJADeviceExec> valueGetter {};
+   auto* rawValues = valueGetter.getRawArrayData(values);
+
+   CHAIDataGetter<OffsetT, RAJADeviceExec> offsetGetter {};
+   const auto* rawOffsets = offsetGetter.getConstRawArrayData(offsets);
+
+   care::host_device_ptr<KeyT> keyResult(numItems);
+   auto* rawKeyResult = keyGetter.getRawArrayData(keyResult);
+
+   care::host_device_ptr<ValueT> valueResult(numItems);
+   auto* rawValueResult = valueGetter.getRawArrayData(valueResult);
+
+   size_t tempStorageBytes = 0;
+   cub::DeviceSegmentedSort::SortPairs(
+      nullptr, tempStorageBytes, rawKeys, rawKeyResult,
+      rawValues, rawValueResult, numItems, numSegments,
+      rawOffsets, rawOffsets + 1);
+
+   CHAIDataGetter<char, RAJADeviceExec> charGetter {};
+   care::host_device_ptr<char> tempStorage(tempStorageBytes);
+   auto* rawTempStorage = charGetter.getRawArrayData(tempStorage);
+   cub::DeviceSegmentedSort::SortPairs(
+      rawTempStorage, tempStorageBytes, rawKeys, rawKeyResult,
+      rawValues, rawValueResult, numItems, numSegments,
+      rawOffsets, rawOffsets + 1);
+
+   tempStorage.free();
+
+   if (keys.isSlice() || values.isSlice()) {
+      care::host_device_ptr<const KeyT> keySource = keyResult;
+      care::host_device_ptr<const ValueT> valueSource = valueResult;
+
+      CARE_STREAM_LOOP(i, 0, numItems) {
+         keys[i] = keySource[i];
+         values[i] = valueSource[i];
+      } CARE_STREAM_LOOP_END
+
+      keyResult.free();
+      valueResult.free();
+   } else {
+      keys.free();
+      values.free();
+      keys = std::move(keyResult);
+      values = std::move(valueResult);
    }
 }
 
--- src/care/sort.h
+++ src/care/sort.h
@@ -21,6 +21,41 @@
 namespace care {
 
 /**
+ * @brief Sort keys in ascending order.
+ * @param keys Keys to sort in place.
+ */
+template <typename KeyT>
+CARE_INLINE void sort(care::host_device_ptr<KeyT>& keys)
+{
+#if defined(__CUDACC__)
+   care::cuda::sort(keys);
+#elif defined(__HIPCC__)
+   care::hip::sort(keys);
+#else
+   care::host::sort(keys);
+#endif
+}
+
+/**
+ * @brief Sort keys in ascending order and apply the same permutation to values.
+ * @param keys Keys to sort in place.
+ * @param values Values associated one-to-one with keys. Must have the same
+ * number of entries as keys.
+ */
+template <typename KeyT, typename ValueT>
+CARE_INLINE void paired_sort(care::host_device_ptr<KeyT>& keys,
+                             care::host_device_ptr<ValueT>& values)
+{
+#if defined(__CUDACC__)
+   care::cuda::paired_sort(keys, values);
+#elif defined(__HIPCC__)
+   care::hip::paired_sort(keys, values);
+#else
+   care::host::paired_sort(keys, values);
+#endif
+}
+
+/**
  * @brief Sort keys in ascending order independently within each segment.
  * @param keys Keys to sort in place.
  * @param offsets Segment boundaries. For N segments, offsets must contain
@@ -40,6 +75,34 @@ CARE_INLINE void segmented_sort(care::host_device_ptr<KeyT>& keys,
    care::hip::segmented_sort(keys, offsets);
 #else
    care::host::segmented_sort(keys, offsets);
+#endif
+}
+
+/**
+ * @brief Sort keys independently within each segment and apply the same
+ * permutation to values.
+ * @param keys Keys to sort in place.
+ * @param values Values associated one-to-one with keys. Must have the same
+ * number of entries as keys.
+ * @param offsets Segment boundaries. For N segments, offsets must contain
+ * N + 1 entries: offsets[i] begins segment i, and offsets[N] marks the end of
+ * the final segment. Segment i is therefore [offsets[i], offsets[i + 1]).
+ * The entries must form a nondecreasing sequence from 0 to keys.size(); that
+ * is, offsets[0] must be 0 and offsets[N] must equal keys.size(). Repeated
+ * entries denote empty segments.
+ */
+template <typename KeyT, typename ValueT, typename OffsetT>
+CARE_INLINE void segmented_paired_sort(
+   care::host_device_ptr<KeyT>& keys,
+   care::host_device_ptr<ValueT>& values,
+   care::host_device_ptr<OffsetT> const& offsets)
+{
+#if defined(__CUDACC__)
+   care::cuda::segmented_paired_sort(keys, values, offsets);
+#elif defined(__HIPCC__)
+   care::hip::segmented_paired_sort(keys, values, offsets);
+#else
+   care::host::segmented_paired_sort(keys, values, offsets);
 #endif
 }
 
